32a8f2d01b60fd66ce34032c90dc892f4b1803dd
GustavoStahl <51406079+GustavoStahl@users.noreply.github.com>
2020-05-18 15:29:40 -0300

    YOLO + Hand Gesture Regognition

--- ContinuousGesturePredictor.py
+++ ContinuousGesturePredictor.py
@@ -7,6 +7,29 @@ import numpy as np
 from PIL import Image
 import cv2
 import imutils
+from pynput.keyboard import Key, Controller
+from yolo import YOLO
+import argparse
+import time
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-n', '--network', default="normal", help='Network Type: normal / tiny / prn')
+ap.add_argument('-d', '--device', default=0, help='Device to use')
+ap.add_argument('-s', '--size', default=256, help='Size for yolo')
+ap.add_argument('-c', '--confidence', default=0.2, help='Confidence for yolo')
+args = ap.parse_args()
+
+if args.network == "normal":
+    print("loading yolo...")
+    yolo = YOLO("models/cross-hands.cfg", "models/cross-hands.weights", ["hand"])
+elif args.network == "prn":
+    print("loading yolo-tiny-prn...")
+    yolo = YOLO("models/cross-hands-tiny-prn.cfg", "models/cross-hands-tiny-prn.weights", ["hand"])
+else:
+    print("loading yolo-tiny...")
+    yolo = YOLO("models/cross-hands-tiny.cfg", "models/cross-hands-tiny.weights", ["hand"])
+
+keyboard = Controller()
 
 # global variables
 bg = None
@@ -16,7 +39,7 @@ def resizeImage(imageName):
     img = Image.open(imageName)
     wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
-    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img = img.resize((basewidth,89), Image.ANTIALIAS)
     img.save(imageName)
 
 def run_avg(image, aWeight):
@@ -29,10 +52,17 @@ def run_avg(image, aWeight):
     # compute weighted average, accumulate it and update the background
     cv2.accumulateWeighted(image, bg, aWeight)
 
-def segment(image, threshold=25):
-    global bg
+def segment(image, top, left, bottom, right, threshold=20):
+
+    image = image[top:bottom, left:right]
+
+    image = cv2.resize(image, (215, 240), interpolation=cv2.INTER_AREA)
+    background = bg[top:bottom, left:right]
+    background = cv2.resize(background, (215, 240), interpolation=cv2.INTER_AREA)
+    cv2.imshow("teste", image)
+
     # find the absolute difference between background and current frame
-    diff = cv2.absdiff(bg.astype("uint8"), image)
+    diff = cv2.absdiff(background.astype("uint8"), image)
 
     # threshold the diff image so that we get the foreground
     thresholded = cv2.threshold(diff,
@@ -60,15 +90,14 @@ def main():
     # get the reference to the webcam
     camera = cv2.VideoCapture(0)
 
-    # region of interest (ROI) coordinates
-    top, right, bottom, left = 10, 350, 225, 590
-
     # initialize num of frames
     num_frames = 0
     start_recording = False
 
+    (grabbed, frame) = camera.read()
     # keep looping, until interrupted
-    while(True):
+    while grabbed:
+        time.sleep(0.025)
         # get the current frame
         (grabbed, frame) = camera.read()
 
@@ -84,20 +113,34 @@ def main():
         # get the height and width of the frame
         (height, width) = frame.shape[:2]
 
-        # get the ROI
-        roi = frame[top:bottom, right:left]
+        # region of interest (ROI) coordinates
+        top, left, bottom, right = 10, 350, 225, 590
 
-        # convert the roi to grayscale and blur it
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        # get the ROI
+        roi = frame.copy() #[top:bottom, right:left]
 
         # to get the background, keep looking till a threshold is reached
         # so that our running average model gets calibrated
         if num_frames < 30:
+            # convert the roi to grayscale and blur it
+            gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (7, 7), 0)
             run_avg(gray, aWeight)
         else:
+            width, height, inference_time, results = yolo.inference(roi)
+            for detection in results:
+                id, name, confidence, x, y, w, h = detection
+                if x<0 : x=0
+                if y<0 : y=0
+                top, left, bottom, right = y, x, y+h, x+w
+                cv2.rectangle(clone, (x-15, y-15), (x + w + 15, y + h + 15), (0, 255, 255), 2)
+            
+            # convert the roi to grayscale and blur it
+            gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (7, 7), 0)                
+
             # segment the hand region
-            hand = segment(gray)
+            hand = segment(gray, top, left, bottom, right)
 
             # check whether hand region is segmented
             if hand is not None:
@@ -106,16 +149,17 @@ def main():
                 (thresholded, segmented) = hand
 
                 # draw the segmented region and display the frame
-                cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                # cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
                 if start_recording:
                     cv2.imwrite('Temp.png', thresholded)
                     resizeImage('Temp.png')
-                    predictedClass, confidence = getPredictedClass()
-                    showStatistics(predictedClass, confidence)
+                    print("Started prediction\nStarted prediction\nStarted prediction")
+                    # predictedClass, confidence = getPredictedClass()
+                    # showStatistics(predictedClass, confidence)
                 cv2.imshow("Thesholded", thresholded)
 
         # draw the segmented hand
-        cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
+        # cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
 
         # increment the number of frames
         num_frames += 1
@@ -133,76 +177,85 @@ def main():
         if keypress == ord("s"):
             start_recording = True
 
-def getPredictedClass():
-    # Predict
-    image = cv2.imread('Temp.png')
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    prediction = model.predict([gray_image.reshape(89, 100, 1)])
-    return np.argmax(prediction), (np.amax(prediction) / (prediction[0][0] + prediction[0][1] + prediction[0][2]))
+# def getPredictedClass():
+#     # Predict
+#     image = cv2.imread('Temp.png')
+#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     prediction = model.predict([gray_image.reshape(89, 100, 1)])
+#     return np.argmax(prediction), (np.amax(prediction) / (prediction[0][0] + prediction[0][1] + prediction[0][2]))
 
-def showStatistics(predictedClass, confidence):
+# def showStatistics(predictedClass, confidence):
 
-    textImage = np.zeros((300,512,3), np.uint8)
-    className = ""
+#     textImage = np.zeros((300,512,3), np.uint8)
+#     className = ""
 
-    if predictedClass == 0:
-        className = "Swing"
-    elif predictedClass == 1:
-        className = "Palm"
-    elif predictedClass == 2:
-        className = "Fist"
+#     if predictedClass == 0:
+#         className = "Swing"
+#         keyboard.release('w')
+#         keyboard.release('d')
+#         keyboard.press('a')
+#     elif predictedClass == 1:
+#         className = "Palm"
+#         keyboard.release('a')
+#         keyboard.release('d')
+#         keyboard.press('w')
+#     elif predictedClass == 2:
+#         className = "Fist"
+#         keyboard.release('a')
+#         keyboard.release('w')
+#         keyboard.press('d')
 
-    cv2.putText(textImage,"Pedicted Class : " + className, 
-    (30, 30), 
-    cv2.FONT_HERSHEY_SIMPLEX, 
-    1,
-    (255, 255, 255),
-    2)
+#     cv2.putText(textImage,"Pedicted Class : " + className, 
+#     (30, 30), 
+#     cv2.FONT_HERSHEY_SIMPLEX, 
+#     1,
+#     (255, 255, 255),
+#     2)
 
-    cv2.putText(textImage,"Confidence : " + str(confidence * 100) + '%', 
-    (30, 100), 
-    cv2.FONT_HERSHEY_SIMPLEX, 
-    1,
-    (255, 255, 255),
-    2)
-    cv2.imshow("Statistics", textImage)
-
-
+#     cv2.putText(textImage,"Confidence : " + str(confidence * 100) + '%', 
+#     (30, 100), 
+#     cv2.FONT_HERSHEY_SIMPLEX, 
+#     1,
+#     (255, 255, 255),
+#     2)
+#     cv2.imshow("Statistics", textImage)
 
 
-# Model defined
-tf.reset_default_graph()
-convnet=input_data(shape=[None,89,100,1],name='input')
-convnet=conv_2d(convnet,32,2,activation='relu')
-convnet=max_pool_2d(convnet,2)
-convnet=conv_2d(convnet,64,2,activation='relu')
-convnet=max_pool_2d(convnet,2)
 
-convnet=conv_2d(convnet,128,2,activation='relu')
-convnet=max_pool_2d(convnet,2)
 
-convnet=conv_2d(convnet,256,2,activation='relu')
-convnet=max_pool_2d(convnet,2)
+# # Model defined
+# tf.reset_default_graph()
+# convnet=input_data(shape=[None,89,100,1],name='input')
+# convnet=conv_2d(convnet,32,2,activation='relu')
+# convnet=max_pool_2d(convnet,2)
+# convnet=conv_2d(convnet,64,2,activation='relu')
+# convnet=max_pool_2d(convnet,2)
 
-convnet=conv_2d(convnet,256,2,activation='relu')
-convnet=max_pool_2d(convnet,2)
+# convnet=conv_2d(convnet,128,2,activation='relu')
+# convnet=max_pool_2d(convnet,2)
 
-convnet=conv_2d(convnet,128,2,activation='relu')
-convnet=max_pool_2d(convnet,2)
+# convnet=conv_2d(convnet,256,2,activation='relu')
+# convnet=max_pool_2d(convnet,2)
 
-convnet=conv_2d(convnet,64,2,activation='relu')
-convnet=max_pool_2d(convnet,2)
+# convnet=conv_2d(convnet,256,2,activation='relu')
+# convnet=max_pool_2d(convnet,2)
 
-convnet=fully_connected(convnet,1000,activation='relu')
-convnet=dropout(convnet,0.75)
+# convnet=conv_2d(convnet,128,2,activation='relu')
+# convnet=max_pool_2d(convnet,2)
 
-convnet=fully_connected(convnet,3,activation='softmax')
+# convnet=conv_2d(convnet,64,2,activation='relu')
+# convnet=max_pool_2d(convnet,2)
 
-convnet=regression(convnet,optimizer='adam',learning_rate=0.001,loss='categorical_crossentropy',name='regression')
+# convnet=fully_connected(convnet,1000,activation='relu')
+# convnet=dropout(convnet,0.75)
 
-model=tflearn.DNN(convnet,tensorboard_verbose=0)
+# convnet=fully_connected(convnet,3,activation='softmax')
 
-# Load Saved Model
-model.load("TrainedModel/GestureRecogModel.tfl")
+# convnet=regression(convnet,optimizer='adam',learning_rate=0.001,loss='categorical_crossentropy',name='regression')
+
+# model=tflearn.DNN(convnet,tensorboard_verbose=0)
+
+# # Load Saved Model
+# model.load("TrainedModel/GestureRecogModel.tfl")
 
 main()
